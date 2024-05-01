56394021bed7d3eec8c8b0bf0fce4c73aea24312
Ajay Yadav <yajay04112000@gmail.com>
2024-05-01 12:05:56 +0530

    minor change in global error handler-fix

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -10,7 +10,6 @@ const globalErrorHandler = (
 ) => {
   const statusCode = err.statusCode || 500;
 
-  next();
   return res.status(statusCode).json({
     message: err.message,
     errorStack: configuration.env === "development" ? err.stack : "",
